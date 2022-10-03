5e02aa747b98d271f9391e445bccb8561ec55173
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-10-03 19:58:57 +0300

    some scss with header + burger menu and react-icons

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import NavBar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <>
-      <NavBar />
-      {/* <Home />
-      <Main />
-      <Footer /> */}
+      <Routes>
+        <Route path="/" exact element={<NavBar />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
     </>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,55 @@
 import React from "react";
 import "./Navbar.css";
-
+import { Link } from "react-router-dom";
+import { GiFishing } from "react-icons/gi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { BiDotsHorizontal } from "react-icons/bi";
 const Navbar = () => {
   return (
     <section className="navBarSection">
       <header className="header flex">
         <div className="logoDiv">
-          <a href="#" className="logo flex">
-            <h1>Fishing.</h1>
-          </a>
+          <Link to="/" className="logo flex">
+            <h1>
+              {" "}
+              <GiFishing className="icon" />
+              Fishing.
+            </h1>
+          </Link>
+        </div>
+        <div className="navBar">
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navList">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navList">
+                About
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navList">
+                News
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navList">
+                Price
+              </a>
+            </li>
+            <button className="btn">
+              <a href="#">Click me!</a>
+            </button>
+          </ul>
+          <div className="closeNavBar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
+
+        <div className="togglerNavBar">
+          <BiDotsHorizontal className="icon-dots" />
         </div>
       </header>
     </section>
